ae26ac01478e97220181b01f7dc44b6aed1f2dfe
Christian Callau <christian.callau@cabify.com>
2022-07-01 04:58:43 +0200

    clean scripts

--- scripts/calibrate.ts
+++ scripts/calibrate.ts
@@ -11,11 +11,11 @@ async function readConfig() {
 			const [key, value] = line.split('=')
 			config[key] = value
 			return config
-		}, {})
+		}, <Record<string, string>>{})
 	return config
 }
 
-async function writeConfig(config) {
+async function writeConfig(config: Record<string, string>) {
 	const config_as_text = Object.entries(config)
 		.map(entry => entry.join('='))
 		.join('\n')
@@ -31,19 +31,19 @@ async function main() {
 		'the following is for calibrating the region from the screen from which to read the pressure readings'
 	)
 
-	await prompt.get(
-		'keeping the terminal window active hover the mouse cursor over the top left point of the desired region and press enter'
-	)
+	await prompt.get([
+		'keeping the terminal window active hover the mouse cursor over the top left point of the desired region and press enter',
+	])
 	const position1 = await mouse.getPosition()
-	config['POS_1_X'] = position1.x
-	config['POS_1_Y'] = position1.y
+	config['POS_1_X'] = position1.x.toString()
+	config['POS_1_Y'] = position1.y.toString()
 
-	await prompt.get(
-		'keeping the terminal window active hover the mouse cursor over the bottom right point of the desired region and press enter'
-	)
+	await prompt.get([
+		'keeping the terminal window active hover the mouse cursor over the bottom right point of the desired region and press enter',
+	])
 	const position2 = await mouse.getPosition()
-	config['POS_2_X'] = position2.x
-	config['POS_2_Y'] = position2.y
+	config['POS_2_X'] = position2.x.toString()
+	config['POS_2_Y'] = position2.y.toString()
 
 	await writeConfig(config)
 	console.log('configuration written successfully')
